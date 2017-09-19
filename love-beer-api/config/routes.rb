7a454caf9f8d8f847291926b128daad884fae82f
Howard DeVennish <howarddevennish@gmail.com>
2017-09-19 12:33:32 -0400

    add comments routes, add url to beers

--- love-beer-api/config/routes.rb
+++ love-beer-api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :beers, except: [:new, :edit]
+    resources :beers, except: [:new, :edit] do
+      resources :comments, except: [:new, :edit, :show]
+    end
   end
 end
