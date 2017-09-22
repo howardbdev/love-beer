89099e6e0d27d3dd54732c1c6cc45e805a811cca
Howard DeVennish <howarddevennish@gmail.com>
2017-09-22 11:49:48 -0400

    flesh out beers and brewers controller

--- love-beer-api/app/controllers/api/beers_controller.rb
+++ love-beer-api/app/controllers/api/beers_controller.rb
@@ -1,16 +1,17 @@
 class API::BeersController < ApplicationController
+  before_action :get_beer, only: [:show, :update, :delete]
+
   def index
     render json: Beer.all
   end
 
   def show
-    beer = Beer.where("id = #{params[:id]}")
     render json: beer
   end
 
   def create
     beer = Beer.new(beer_params)
-    
+
     if beer.save
       render json: beer
     else
@@ -20,17 +21,29 @@ class API::BeersController < ApplicationController
   end
 
   def update
-
+    if beer.update(beer_params)
+      render json: beer
+    else
+      render json: {error: "There was a problem updating your beer " + beer.errors.full_messages.to_sentence}
+    end
   end
 
   def delete
-
+    if beer.delete
+      render json: Beer.all
+    else
+      render json: {error: "There was a problem deleting that beer " + beer.errors.full_messages.to_sentence}
+    end
   end
 
   private
 
   def beer_params
     params.require(:beer).permit(:name, :brewer_id, :style, :description, :url, :image_url, :upvotes)
+  end
+
+  def get_beer
+    beer = Beer.where("id = #{params[:id]}")
   end
 
 end
--- love-beer-api/app/controllers/api/brewers_controller.rb
+++ love-beer-api/app/controllers/api/brewers_controller.rb
@@ -1,5 +1,49 @@
 class API::BrewersController < ApplicationController
+  before_action :get_brewer, only: [:show, :update, :delete]
+
   def index
     render json: Brewer.all
   end
+
+  def show
+    render json: brewer
+  end
+
+  def create
+    brewer = Brewer.new(brewer_params)
+
+    if brewer.save
+      render json: brewer
+    else
+      render json: {error: "There was a problem saving your brewer " + brewer.errors.full_messages.to_sentence}
+    end
+
+  end
+
+  def update
+    if brewer.update(brewer_params)
+      render json: brewer
+    else
+      render json: {error: "There was a problem updating your brewer " + brewer.errors.full_messages.to_sentence}
+    end
+  end
+
+  def delete
+    if brewer.delete
+      render json: Brewer.all
+    else
+      render json: {error: "There was a problem deleting that brewer " + brewer.errors.full_messages.to_sentence}
+    end
+  end
+
+  private
+
+  def brewer_params
+    params.require(:brewer).permit(:name, :location, :established, :url)
+  end
+
+  def get_brewer
+    brewer = Brewer.where("id = #{params[:id]}")
+  end
+
 end
