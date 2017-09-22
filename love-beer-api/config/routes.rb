f306f2bf745398be8054e45c89fdaf6aec25e87c
Howard DeVennish <howarddevennish@gmail.com>
2017-09-22 10:41:23 -0400

    add brewers controller, routes and rearrange controllers file structure

--- love-beer-api/config/routes.rb
+++ love-beer-api/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :beers, except: [:new, :edit] do
       resources :comments, except: [:new, :edit, :show]
     end
+    resources :brewers, except: [:new, :edit]
   end
 end
