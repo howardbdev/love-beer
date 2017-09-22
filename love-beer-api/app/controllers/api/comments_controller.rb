d845d8ec6f1e6a5c77b725c3ff2bc2ba6f3f73bb
Howard DeVennish <howarddevennish@gmail.com>
2017-09-22 12:36:46 -0400

    flesh out comments controller

--- love-beer-api/app/controllers/api/comments_controller.rb
+++ love-beer-api/app/controllers/api/comments_controller.rb
@@ -1,5 +1,39 @@
 class API::CommentsController < ApplicationController
-  def index
-    render json: {comments_index: "I'm the comments index"}
+  before_action :get_comment, only: [:update, :delete]
+
+  def create
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: comment
+    else
+      render json: {error: comment.errors.full_messages.to_sentence}
+    end
+  end
+
+  def update
+    if comment.update(comment_params)
+      render json: comment
+    else
+      render json: {error: comment.errors.full_messages.to_sentence}
+    end
+  end
+
+  def delete
+    if comment.delete
+      render json: {comment_was: "deleted"}
+    else
+      render json: {error: comment.errors.full_messages.to_sentence}
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
+  end
+
+  def get_comment
+    comment = Comment.where("id = #{params[:id]}")
   end
 end
