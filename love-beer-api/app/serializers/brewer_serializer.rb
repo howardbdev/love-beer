f8f1c612a0a345813a1c3cb120211578990067a2
Howard DeVennish <howarddevennish@gmail.com>
2017-09-22 11:07:59 -0400

    update brewer serializer

--- love-beer-api/app/serializers/brewer_serializer.rb
+++ love-beer-api/app/serializers/brewer_serializer.rb
@@ -1,5 +1,5 @@
 class BrewerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :beers
+  attributes :id, :name, :location, :beers, :established, :url
 
   def beers
     Beer.where("brewer_id = #{self.object.id}").first.name
