c55bb14b8c6631d5fb67b113742d441dc74c27d4
Howard DeVennish <howarddevennish@gmail.com>
2017-09-27 09:51:55 -0400

    remove header and old Beer component code

--- love-beer-api/client/src/containers/App.js
+++ love-beer-api/client/src/containers/App.js
@@ -5,11 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div>
           <img src="http://images.freeimages.com/images/small-previews/9c1/pint-of-beer-1543005.jpg" className="App-logo" alt="I Love Beer - Do You?" />
           <h2>Love Beer</h2>
         </div>
-        <div><Beers/></div>
       </div>
     );
   }
