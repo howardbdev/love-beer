ba7a62c38de971c48cfae92e050b1d169f8720c1
Howard DeVennish <howarddevennish@gmail.com>
2017-09-28 09:52:57 -0400

    add BeerForm component

---
+++ love-beer-api/client/src/containers/BeerForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BeerForm extends Component {
+
+  render() {
+    return (
+      <div>
+        Enter a new beer
+        <form action="">
+          Here is the form
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BeerForm;
--- love-beer-api/client/src/containers/Beers.js
+++ love-beer-api/client/src/containers/Beers.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Beers.css';
 import BeerCard from '../components/BeerCard'
+import BeerForm from './BeerForm'
 
-const Beers = (props) => (
-    <div>
-      {props.beers.map((beer, i) => <BeerCard beer={beer} key={i} />
-      )}
-    </div>
-  )
+class Beers extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.beers.map(beer => <BeerCard beer={beer} key={beer.id} />
+        )}
+        <BeerForm/>
+      </div>
+    );
+  }
+}
 
 
 export default Beers;
