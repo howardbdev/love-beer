5c1f066c0d8bad1f9ab21fbc7473dcd614236f43
Howard DeVennish <howarddevennish@gmail.com>
2017-10-12 11:26:15 -0400

    remove console.logs and clean up params

--- love-beer-api/client/src/containers/Beer.js
+++ love-beer-api/client/src/containers/Beer.js
@@ -4,20 +4,15 @@ import { getBeer } from '../actions/beers';
 import './Beers.css';
 import BeerShow from '../components/BeerShow'
 
-// console.log("PARAMS -->", params["match"]["params"]["id"])
 class Beer extends Component {
   constructor(props) {
     super()
-    console.log("PROPS MATCH PARAMS ID IS : ",props["match"]["params"]["id"])
-    this.beer_id = props["match"]["params"]["id"]
-    console.log("beer_id is : ", this.beer_id)
+    this.beer_id = props.match.params.id
   }
   componentDidMount() {
-    console.log("BEER ID IS: ", this.beer_id)
     this.props.getBeer(this.beer_id)
   }
   render() {
-    console.log("IN BEER RENDER", this.props.beer)
     return (
       <div>
         <BeerShow beer={this.props.beer}/>
